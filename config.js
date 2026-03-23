877181f5942161d711306bb87f6d925ffaea3780
grsearch <grsearch@gmail.com>
2026-03-23 10:44:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 module.exports = {
   birdeye: {
     apiKey:  process.env.BIRDEYE_API_KEY || '',
-    wsUrl:   'wss://public-api.birdeye.so/socket',
+    wsUrl:   'wss://public-api.birdeye.so/socket', // birdeyeWs.js 会自动追加 /solana
     restUrl: 'https://public-api.birdeye.so',
   },
   helius: {
